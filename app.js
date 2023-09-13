4c361c168a7d54940bb6b5a256e58a47137c138d
owen-sellner <owen.sellner@gmail.com>
2023-09-13 17:53:38 -0400

    add functions for checkboxes

--- app.js
+++ app.js
@@ -5,17 +5,28 @@ const MAX_LENGTH = 250;
 const API_URL = `https://api.quotable.io/random?minLength=${MIN_LENGTH}&maxLength=${MAX_LENGTH}`
 
 async function getQuote(url) {
+
+    // Get response from API
     const res = await fetch(url);
 
+    // Get data as JSON format and extract the content element
     const data = await res.json();
     quoteText = data.content;
-    console.log(quoteText);
 
+    // Check if the check boxes are checked
+    if (!document.getElementById("include-uppercase").checked) {
+        quoteText = quoteText.toLowerCase();
+    }
+
+    if (!document.getElementById("include-punctuation").checked) {
+        quoteText = removePunctuation(quoteText);
+    }
+
+    // Add each character from the quote as a span element
     for(let i = 0; i < quoteText.length; i++) {
         const textBox = document.querySelector('.textbox-text');
         const spanItem = document.createElement('span');
         spanItem.innerText = quoteText[i];
-        console.log(spanItem);
         textBox.append(spanItem);
     }
 }
@@ -23,6 +34,16 @@ async function getQuote(url) {
 getQuote(API_URL);
 
 
+// Removes the punctuation from the quote
+function removePunctuation(quote) {
+    const punctuation = [".", "\?", "!", ",", ";", ":", "—", "-", "(", ")", "[", "]", "{", "}", "\'", "\""];
+
+    for (let i = 0; i < punctuation.length; i++) {
+        quote = quote.replace(/[^a-zA-Z0-9\s]/g, "");
+    }
+
+    return quote;
+}
 
 // Toggle focus on the text input when the user clicks on the textwrapper
 function toggleInputFocus() {
