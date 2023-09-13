8f9497cfd93e4d0934d3daa64630259d5728345e
owen-sellner <owen.sellner@gmail.com>
2023-09-13 16:58:56 -0400

    add api fetch

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+// Getting the text data from API
+const MIN_LENGTH = 100;
+const MAX_LENGTH = 200;
+const API_URL = `https://api.quotable.io/random?minLength=${MIN_LENGTH}&maxLength=${MAX_LENGTH}`
 
+async function getQuote(url) {
+    const res = await fetch(url);
+
+    const data = await res.json();
+    const quoteText = data.content;
+    console.log(quoteText);
+}
+
+getQuote(API_URL)
 
 // Toggle focus on the text input when the user clicks on the textwrapper
 function toggleInputFocus() {
